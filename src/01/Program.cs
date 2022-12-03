be574662e8a0eaad955be2813a4a99e7b9b0917c
Radek Rous <radek.rous@riganti.cz>
2022-12-03 12:27:09 +0100

    Day 1

--- src/01/Program.cs
+++ src/01/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = File.ReadAllLines("input.txt");
+List<int> sums = new()
+{
+    0
+};
+int index = 0;
+foreach (var line in lines)
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        sums.Add(0);
+        index++;
+        continue;
+    }
+
+    sums[index] += int.Parse(line);
+}
+
+Console.WriteLine(sums.Max());
+var sum = sums.OrderByDescending(x=>x).Take(3).Sum();
+Console.WriteLine(sum);
