a7bb316c7d4d7e92dca050e9ae2fecb6671e6eb1
Radek Rous <radek.rous@riganti.cz>
2022-12-03 13:05:12 +0100

    Day 3

---
+++ src/03/Program.cs
@@ -0,0 +1,39 @@
+﻿//var input = "inputTest.txt";
+var input = "input.txt";
+
+var lines = File.ReadAllLines(input);
+
+var sum = 0;
+foreach (var line in lines)
+{
+    var halfLength = line.Length / 2;
+    var first = line[..halfLength];
+    var second = line[halfLength..];
+
+    var intersect = first.ToCharArray().Intersect(second.ToCharArray()).Single();
+
+    var priority = GetPriority(intersect);
+    sum += priority;
+}
+
+// part 1
+Console.WriteLine(sum);
+
+var sum2 = 0;
+for (int i = 0; i < lines.Length; i += 3)
+{
+    var line1 = lines[i];
+    var line2 = lines[i + 1];
+    var line3 = lines[i + 2];
+
+    var intersect = line1.ToCharArray().Intersect(line2.ToCharArray()).Intersect(line3.ToCharArray()).Single();
+    var priority = GetPriority(intersect);
+    sum2 += priority;
+}
+
+Console.WriteLine(sum2);
+
+int GetPriority(char c)
+{
+    return c > 96 ? c - 96 : c - 38;
+}
