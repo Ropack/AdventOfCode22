33ddeb6c40f98f2e67c35ecc93f48403292a418c
Radek Rous <radek.rous@riganti.cz>
2022-12-08 07:35:01 +0100

    Day 6

---
+++ src/06/Program.cs
@@ -0,0 +1,23 @@
+﻿var input = "input.txt";
+var lines = File.ReadAllLines(input);
+
+var line = lines[0];
+//var line = "bvwbjplbgvbhsrlpgdmjqwftvncz"; // 5
+//var line = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"; // 11
+var part1 = GetIndexOfNDistinctCharacters(line, 4);
+Console.WriteLine(part1);
+
+var part2 = GetIndexOfNDistinctCharacters(line, 14);
+Console.WriteLine(part2);
+
+int GetIndexOfNDistinctCharacters(string s, int n)
+{
+    for (int i = n; i < s.Length; i++)
+    {
+        if (s.Substring(i - n, n).ToCharArray().Distinct().Count() == n)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
