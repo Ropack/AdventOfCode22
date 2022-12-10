d481c90920cece05af74f483bff5714d8306676c
Radek Rous <radek.rous@riganti.cz>
2022-12-10 12:11:28 +0100

    Day 10

---
+++ src/10/Program.cs
@@ -0,0 +1,58 @@
+﻿var input = "input.txt";
+//var input = "test-input.txt";
+var lines = File.ReadAllLines(input);
+
+var cycle = 1;
+var registerX = 1;
+var sum = 0;
+foreach (var line in lines)
+{
+    CheckSignificantCycles();
+    Draw();
+    if (line == "noop")
+    {
+
+    }
+    else
+    {
+        var number = int.Parse(line.Split(" ")[1]);
+        cycle++;
+        CheckSignificantCycles();
+        Draw();
+        registerX += number;
+    }
+
+    cycle++;
+}
+
+Console.WriteLine();
+Console.WriteLine(sum);
+
+void CheckSignificantCycles()
+{
+    if (cycle is 20 or 60 or 100 or 140 or 180 or 220)
+    {
+        sum += registerX * cycle;
+    }
+}
+
+void Draw()
+{
+    var pos1 = registerX - 1;
+    var pos2 = registerX ;
+    var pos3 = registerX +1;
+    var renderPos = (cycle-1) % 40;
+    if (renderPos == 0)
+    {
+        Console.WriteLine();
+    }
+
+    if (renderPos == pos1 || renderPos == pos2 || renderPos == pos3)
+    {
+        Console.Write("#");
+    }
+    else
+    {
+        Console.Write(".");
+    }
+};
