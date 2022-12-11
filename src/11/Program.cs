0d256cbbdeaa69dc3674f2f2624af55f55fe9306
Radek Rous <radek.rous@riganti.cz>
2022-12-11 15:24:17 +0100

    Day 11 part 2 WIP

--- src/11/Program.cs
+++ src/11/Program.cs
@@ -1,4 +1,8 @@
-﻿var input = "input.txt";
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+
+var input = "input.txt";
 //var input = "test-input.txt";
 var lines = File.ReadAllLines(input);
 
@@ -15,13 +19,13 @@ foreach (var line in lines)
 
     if (line.StartsWith("Monkey "))
     {
-        currentMonkey.Id = Convert.ToInt32(line.Split(" ")[1][..1]);
+        currentMonkey.Id = BigInteger.Parse(line.Split(" ")[1][..1]);
     }
 
     if (line.StartsWith("  Starting items:"))
     {
         var itemsString = line.Split("  Starting items: ")[1];
-        currentMonkey.Items = itemsString.Split(", ").Select(int.Parse).ToList();
+        currentMonkey.Items = itemsString.Split(", ").Select(x => new Item(BigInteger.Parse(x))).ToList();
     }
 
     if (line.StartsWith("  Operation:"))
@@ -30,29 +34,80 @@ foreach (var line in lines)
         var operation = expression.Split(" ")[0];
         var operand = expression.Split(" ")[1];
 
-        int OperationFunc(int a, int b) => operation == "+" ? a + b : a * b;
+        BigInteger OperationFunc(BigInteger a, BigInteger b) => operation == "+" ? a + b : a * b;
+        BigInteger OperationFuncBig(BigInteger a, BigInteger b) => operation == "+" ? a + b : a * b;
 
-        currentMonkey.Operation = x => OperationFunc(x, operand == "old" ? x : int.Parse(operand));
+        currentMonkey.Operation = x =>
+        {
+            foreach (var dictionaryKey in x.Dictionary.Keys)
+            {
+                if (operand != "old")
+                {
+                    x.Dictionary[dictionaryKey] = OperationFunc(x.Dictionary[dictionaryKey], BigInteger.Parse(operand));
+                }
+                else
+                {
+                    x.Dictionary[dictionaryKey] = OperationFunc(x.Dictionary[dictionaryKey], x.Dictionary[dictionaryKey]);
+                }
+            }
+
+            //if (operand != "old")
+            //{
+            //    x.Current = OperationFuncBig(x.Current, BigInteger.Parse(operand));
+            //}
+            //else
+            //{
+            //    x.Current = OperationFuncBig(x.Current, x.Current);
+            //}
+
+            return x;
+        };
     }
 
     if (line.StartsWith("  Test: "))
     {
         var divisibleBy = int.Parse(line.Split("by ")[1]);
-        currentMonkey.Test = x => x % divisibleBy == 0;
+        currentMonkey.Test = x =>
+        {
+            var isDivisible = x.Dictionary[divisibleBy] % divisibleBy == 0;
+
+            if (isDivisible != (x.Current % divisibleBy == 0))
+            {
+                Debugger.Break();
+            }
+
+            foreach (var dictionaryKey in x.Dictionary.Keys)
+            {
+                if (x.Dictionary[dictionaryKey] % dictionaryKey == 0)
+                {
+                    x.Dictionary[dictionaryKey] = dictionaryKey;
+                }
+            }
+
+
+            if (isDivisible)
+            {
+                //x.Dictionary[divisibleBy] = 0;
+                return true;
+            }
+
+            return false;
+        };
     }
-    
+
     if (line.StartsWith("    If true: "))
     {
-        currentMonkey.TargetWhenTrue = int.Parse(line.Split("monkey ")[1]);
+        currentMonkey.TargetWhenTrue = BigInteger.Parse(line.Split("monkey ")[1]);
     }
     if (line.StartsWith("    If false: "))
     {
-        currentMonkey.TargetWhenFalse = int.Parse(line.Split("monkey ")[1]);
+        currentMonkey.TargetWhenFalse = BigInteger.Parse(line.Split("monkey ")[1]);
     }
 }
 monkeys.Add(currentMonkey);
 
-for (int i = 0; i < 20; i++)
+//for (int i = 0; i < 20; i++)
+for (int i = 0; i < 10000; i++)
 {
     foreach (var monkey in monkeys)
     {
@@ -60,12 +115,22 @@ for (int i = 0; i < 20; i++)
         {
             var newValue = monkey.Operation(monkeyItem);
             monkey.InspectionCounter++;
-            newValue /= 3;
+
+            //foreach (var dictionaryKey in newValue.Dictionary.Keys)
+            //{
+            //    newValue.Dictionary[dictionaryKey] /= 3;
+            //}
+
             var target = monkey.Test(newValue) ? monkey.TargetWhenTrue : monkey.TargetWhenFalse;
             monkeys.Single(x => x.Id == target).Items.Add(newValue);
         }
 
-        monkey.Items = new List<int>();
+        monkey.Items = new List<Item>();
+    }
+
+    if (i % 10 == 0)
+    {
+        Console.WriteLine($"Round {i}");
     }
 }
 
@@ -75,11 +140,37 @@ Console.WriteLine(monkeyBusiness);
 
 class Monkey
 {
-    public int Id { get; set; }
-    public List<int> Items { get; set; } = new();
-    public Func<int, int> Operation { get; set; }
-    public Func<int, bool> Test { get; set; }
-    public int TargetWhenTrue { get; set; }
-    public int TargetWhenFalse { get; set; }
-    public int InspectionCounter { get; set; } = 0;
+    public BigInteger Id { get; set; }
+    public List<Item> Items { get; set; } = new();
+    public Func<Item, Item> Operation { get; set; }
+    public Func<Item, bool> Test { get; set; }
+    public BigInteger TargetWhenTrue { get; set; }
+    public BigInteger TargetWhenFalse { get; set; }
+    public BigInteger InspectionCounter { get; set; } = 0;
+}
+
+class Item
+{
+    public BigInteger Initial { get; }
+    public Dictionary<int, BigInteger> Dictionary { get; }
+    public BigInteger Current { get; set; }
+
+    public Item(BigInteger initial)
+    {
+        Initial = initial;
+        Dictionary = new Dictionary<int, BigInteger>
+        {
+            [13] = Initial,
+            [19] = Initial,
+            [5] = Initial,
+            [2] = Initial,
+            [17] = Initial,
+            [11] = Initial,
+            [7] = Initial,
+            [3] = Initial,
+
+            //[23] = Initial
+        };
+        Current = initial;
+    }
 }
