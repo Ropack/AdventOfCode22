c01560197d8a504a2a3f76366c708657f3c7ff95
Radek Rous <radek.rous@riganti.cz>
2022-12-12 12:47:32 +0100

    Day 12 part 1 - rewrite DFS to BFS

--- src/12/Program.cs
+++ src/12/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.ComponentModel;
+
 var input = "input.txt";
 //var input = "test-input.txt";
 var lines = File.ReadAllLines(input);
@@ -33,46 +35,31 @@ for (int i = 0; i < sizeY; i++)
 
 var initialArray = new bool[sizeX, sizeY];
 initialArray[startX, startY] = true;
-var result = F(startX, startY, initialArray, int.MaxValue);
-Console.WriteLine(result-1);
+//var result = F(startX, startY, initialArray, int.MaxValue);
 
-int F(int x, int y, bool[,] visited, int currentMin)
+var q = new Queue<(int x, int y, bool[,] visited, int currentLength)>();
+q.Enqueue((startX, startY, initialArray, 0));
+while (q.TryDequeue(out var item))
 {
+    var x = item.x;
+    var y = item.y;
     var current = map[x, y];
-    var results = new List<int>()
-    {
-        currentMin
-    };
-
-    var sumThreshold = 20;
-
+    var visited = item.visited;
     //right
     {
         var newX = x + 1;
         var newY = y;
         if (x < sizeX - 1 && current > map[newX, newY] - 2 && !visited[newX, newY])
         {
-            var array = new bool[sizeX, sizeY];
-            Buffer.BlockCopy(visited, 0, array, 0, visited.Length * sizeof(bool));
-            array[newX, newY] = true;
-            var sum = array.Cast<bool>().Sum(b => b ? 1 : 0);
+            visited[newX, newY] = true;
+            var sum = item.currentLength + 1;
             if (map[newX, newY] == 27)
             {
                 Console.WriteLine($"Found end, returning {sum}");
-                return sum;
+                return;
             }
-            
-            if (sum < currentMin)
-            {
-                if (sum < sumThreshold)
-                {
-                    results.Add(F(newX, newY, array, results.Min()));
-                }
-                else
-                {
-                    Console.WriteLine("Sum is over 120, skipping");
-                }
-            }
+
+            q.Enqueue((newX, newY, visited, sum));
         }
     }
 
@@ -82,58 +69,33 @@ int F(int x, int y, bool[,] visited, int currentMin)
         var newY = y;
         if (x > 0 && current > map[newX, newY] - 2 && !visited[newX, newY])
         {
-            var array = new bool[sizeX, sizeY];
-            Buffer.BlockCopy(visited, 0, array, 0, visited.Length * sizeof(bool));
-            array[newX, newY] = true;
-            var sum = array.Cast<bool>().Sum(b => b ? 1 : 0);
+            visited[newX, newY] = true;
+            var sum = item.currentLength + 1;
             if (map[newX, newY] == 27)
             {
                 Console.WriteLine($"Found end, returning {sum}");
-                return sum;
+                return;
             }
-            
-            if (sum < currentMin)
-            {
-                if (sum < sumThreshold)
-                {
-                    results.Add(F(newX, newY, array, results.Min()));
-                }
-                else
-                {
-                    Console.WriteLine("Sum is over 120, skipping");
-                }
-            }
+
+            q.Enqueue((newX, newY, visited, sum));
         }
     }
 
     // bottom
-
     {
         var newX = x;
         var newY = y + 1;
         if (y < sizeY - 1 && current > map[newX, newY] - 2 && !visited[newX, newY])
         {
-            var array = new bool[sizeX, sizeY];
-            Buffer.BlockCopy(visited, 0, array, 0, visited.Length * sizeof(bool));
-            array[newX, newY] = true;
-            var sum = array.Cast<bool>().Sum(b => b ? 1 : 0);
+            visited[newX, newY] = true;
+            var sum = item.currentLength + 1;
             if (map[newX, newY] == 27)
             {
                 Console.WriteLine($"Found end, returning {sum}");
-                return sum;
+                return;
             }
 
-            if (sum < currentMin)
-            {
-                if (sum < sumThreshold)
-                {
-                    results.Add(F(newX, newY, array, results.Min()));
-                }
-                else
-                {
-                    Console.WriteLine("Sum is over 120, skipping");
-                }
-            }
+            q.Enqueue((newX, newY, visited, sum));
         }
     }
 
@@ -143,30 +105,153 @@ int F(int x, int y, bool[,] visited, int currentMin)
         var newY = y - 1;
         if (y > 0 && current > map[newX, newY] - 2 && !visited[newX, newY])
         {
-            var array = new bool[sizeX, sizeY];
-            Buffer.BlockCopy(visited, 0, array, 0, visited.Length * sizeof(bool));
-            array[newX, newY] = true;
-            var sum = array.Cast<bool>().Sum(b => b ? 1 : 0);
+            visited[newX, newY] = true;
+            var sum = item.currentLength + 1;
             if (map[newX, newY] == 27)
             {
                 Console.WriteLine($"Found end, returning {sum}");
-                return sum;
+                return;
             }
 
-            if (sum < currentMin)
-            {
-                if (sum < sumThreshold)
-                {
-                    results.Add(F(newX, newY, array, results.Min()));
-                }
-                else
-                {
-                    Console.WriteLine("Sum is over 120, skipping");
-                }
-            }
+            q.Enqueue((newX, newY, visited, sum));
         }
     }
-    
-
-    return results.Min();
 }
+
+//Console.WriteLine(result - 1);
+
+//int F(int x, int y, bool[,] visited, int currentMin)
+//{
+//    var current = map[x, y];
+//    Console.WriteLine($"{x} {y} {current}");
+//    var results = new List<int>()
+//    {
+//        currentMin
+//    };
+
+//    var sumThreshold = 300;
+
+//    //right
+//    {
+//        var newX = x + 1;
+//        var newY = y;
+//        if (x < sizeX - 1 && current > map[newX, newY] - 2 && !visited[newX, newY])
+//        {
+//            var array = new bool[sizeX, sizeY];
+//            Buffer.BlockCopy(visited, 0, array, 0, visited.Length * sizeof(bool));
+//            array[newX, newY] = true;
+//            var sum = array.Cast<bool>().Sum(b => b ? 1 : 0);
+//            if (map[newX, newY] == 27)
+//            {
+//                Console.WriteLine($"Found end, returning {sum}");
+//                return sum;
+//            }
+
+//            if (sum < currentMin)
+//            {
+//                if (sum < sumThreshold)
+//                {
+//                    results.Add(F(newX, newY, array, results.Min()));
+//                }
+//                else
+//                {
+//                    Console.WriteLine($"Sum is over {sum}, skipping");
+//                }
+//            }
+//        }
+//    }
+
+//    // left
+//    {
+//        var newX = x - 1;
+//        var newY = y;
+//        if (x > 0 && current > map[newX, newY] - 2 && !visited[newX, newY])
+//        {
+//            var array = new bool[sizeX, sizeY];
+//            Buffer.BlockCopy(visited, 0, array, 0, visited.Length * sizeof(bool));
+//            array[newX, newY] = true;
+//            var sum = array.Cast<bool>().Sum(b => b ? 1 : 0);
+//            if (map[newX, newY] == 27)
+//            {
+//                Console.WriteLine($"Found end, returning {sum}");
+//                return sum;
+//            }
+
+//            if (sum < currentMin)
+//            {
+//                if (sum < sumThreshold)
+//                {
+//                    results.Add(F(newX, newY, array, results.Min()));
+//                }
+//                else
+//                {
+//                    Console.WriteLine($"Sum is over {sum}, skipping");
+//                }
+//            }
+//        }
+//    }
+
+//    // bottom
+
+//    {
+//        var newX = x;
+//        var newY = y + 1;
+//        if (y < sizeY - 1 && current > map[newX, newY] - 2 && !visited[newX, newY])
+//        {
+//            var array = new bool[sizeX, sizeY];
+//            Buffer.BlockCopy(visited, 0, array, 0, visited.Length * sizeof(bool));
+//            array[newX, newY] = true;
+//            var sum = array.Cast<bool>().Sum(b => b ? 1 : 0);
+//            if (map[newX, newY] == 27)
+//            {
+//                Console.WriteLine($"Found end, returning {sum}");
+//                return sum;
+//            }
+
+//            if (sum < currentMin)
+//            {
+//                if (sum < sumThreshold)
+//                {
+//                    results.Add(F(newX, newY, array, results.Min()));
+//                }
+//                else
+//                {
+//                    Console.WriteLine($"Sum is over {sum}, skipping");
+//                }
+//            }
+//        }
+//    }
+
+//    // up
+//    {
+//        var newX = x;
+//        var newY = y - 1;
+//        if (y > 0 && current > map[newX, newY] - 2 && !visited[newX, newY])
+//        {
+//            var array = new bool[sizeX, sizeY];
+//            Buffer.BlockCopy(visited, 0, array, 0, visited.Length * sizeof(bool));
+//            array[newX, newY] = true;
+//            var sum = array.Cast<bool>().Sum(b => b ? 1 : 0);
+//            if (map[newX, newY] == 27)
+//            {
+//                Console.WriteLine($"Found end, returning {sum}");
+//                return sum;
+//            }
+
+//            if (sum < currentMin)
+//            {
+//                if (sum < sumThreshold)
+//                {
+//                    results.Add(F(newX, newY, array, results.Min()));
+//                }
+//                else
+//                {
+//                    Console.WriteLine($"Sum is over {sum}, skipping");
+//                }
+//            }
+//        }
+//    }
+
+
+//    return results.Min();
+//}
